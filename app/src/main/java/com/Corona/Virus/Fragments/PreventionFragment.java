bc6080a54e669247f9ff74b524679b2a2b049930
ShaikAbdulRazzaaq <43449008+ShaikAbdulRazzaaq@users.noreply.github.com>
2020-03-08 19:56:58 +0530

    Initial commit

---
+++ app/src/main/java/com/Corona/Virus/Fragments/PreventionFragment.java
@@ -0,0 +1,21 @@
+package com.Corona.Virus.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.Corona.Virus.R;
+
+public class PreventionFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.prevention_fragment,container,false);
+        return view;
+    }
+}
