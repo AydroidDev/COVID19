29ef29ae8592f0fe39531161aa82e4b42ee626e1
sbmvirdi <shubhamvirdic3@gmail.com>
2020-03-08 23:38:59 +0530

    swipe refresh layout

--- app/src/main/java/com/Corona/Virus/Fragments/NewsFragment.java
+++ app/src/main/java/com/Corona/Virus/Fragments/NewsFragment.java
@@ -1,11 +1,13 @@
 package com.Corona.Virus.Fragments;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +15,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.ListFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.Corona.Virus.Adapters.NewsAdapter;
 import com.Corona.Virus.Interfaces.NewsFeatures;
@@ -29,15 +32,18 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class NewsFragment extends Fragment {
+public class NewsFragment extends Fragment  implements SwipeRefreshLayout.OnRefreshListener{
     private RecyclerView mRec;
+    private SwipeRefreshLayout mSwipe;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.news_fragment,container,false);
+        mSwipe = view.findViewById(R.id.swiperefresh);
         mRec = view.findViewById(R.id.newsFragmentRecycler);
         mRec.setLayoutManager(new LinearLayoutManager(view.getContext()));
         initNews();
+        mSwipe.setOnRefreshListener(this);
         return view;
     }
 
@@ -67,5 +73,19 @@ public class NewsFragment extends Fragment {
         }
         NewsAdapter adapter = new NewsAdapter(getContext(),mList);
         mRec.setAdapter(adapter);
+    }
+
+    @Override
+    public void onRefresh() {
+        Log.e("Refreshed","");
+        initNews();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mSwipe.setRefreshing(false);
+            }
+        },2000);
+
+
     }
 }
--- app/src/main/java/com/Corona/Virus/Utils/Utils.java
+++ app/src/main/java/com/Corona/Virus/Utils/Utils.java
@@ -2,7 +2,7 @@ package com.Corona.Virus.Utils;
 
 public final class Utils {
     public static final String BASE_URL = "http://newsapi.org";
-    public static final String THEME = "coronavirus";
+    public static final String THEME = "Virus";
     public static final String LANGUAGE = "en";
     public static final long PAGE_SIZE = 100;
     public static final String SORT_BY = "publishedAt";
