ed5a7715cf455b4e70a0d0c90c92a0f4322940ee
ShaikAbdulRazzaaq <43449008+ShaikAbdulRazzaaq@users.noreply.github.com>
2020-03-13 11:40:23 +0530

    Color View Added

--- app/src/main/java/com/know/Virus/Adapters/GridAdapter.java
+++ app/src/main/java/com/know/Virus/Adapters/GridAdapter.java
@@ -1,6 +1,7 @@
 package com.know.Virus.Adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,8 +16,10 @@ import androidx.annotation.Nullable;
 
 import com.know.Virus.ModelClasses.gridContent;
 import com.know.Virus.R;
+import com.know.Virus.Utils.Utils;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class GridAdapter extends ArrayAdapter<gridContent> {
     private ArrayList<gridContent> list;
@@ -41,6 +44,8 @@ public class GridAdapter extends ArrayAdapter<gridContent> {
         TextView textView=view.findViewById(R.id.gridText);
         imageView.setImageResource(grid.getImageOfTheVirus());
         textView.setText(grid.getTitleOfTheImage());
+        View designView=view.findViewById(R.id.gridColorDesign);
+        designView.setBackgroundColor(Color.parseColor(Utils.Colors[new Random().nextInt(Utils.Colors.length)]));
         view.setOnClickListener(v -> {
             Toast.makeText(context, "Clicked "+position, Toast.LENGTH_SHORT).show();
             Log.e("Error Handling in grid ","Clciked this "+position);
